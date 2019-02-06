ae98aa783990a4f35b50da34fe1ee80c9808999f
Ken-Mens <kenneth.mensah@gmail.com>
2019-02-06 22:41:16 +0000

    fix repo issues

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - C program which will print exactly "with proper
+  *  grammar, but the outcome is a piece of art,"
+  * use function printf
+  * Return: 0
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
