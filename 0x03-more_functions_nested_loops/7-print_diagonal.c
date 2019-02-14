26523080437da291eb3865fde43518d4147dccc7
Ken-Mens <kenneth.mensah@gmail.com>
2019-02-14 22:23:33 +0000

    draws diagonal line on terminal

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ *@n: integer parameter.
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int rows, columns;
+
+	for (rows = 0; rows < n; rows++)
+	{
+		for (columns = 0; columns <= rows; columns++)
+		{
+		if (rows != columns)
+		{
+		_putchar(' ');
+		}
+		else
+		{
+		_putchar('\\');
+		}
+	}
+	_putchar('\n');
+	}
+	if (n <= 0)
+	_putchar('\n');
+}
