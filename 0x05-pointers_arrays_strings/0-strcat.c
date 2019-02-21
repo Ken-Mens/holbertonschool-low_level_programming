79c98cf4d0f77796acf77c564e094107981025b0
Ken-Mens <kenneth.mensah@gmail.com>
2019-02-21 20:58:31 +0000

    function that concatenates two strings

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: pointer to character array
+ * @src: pointer to character array
+ * Return: pointer to char array dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	char *concat;
+
+	concat = dest;
+	while (*concat)
+	{
+		concat++;
+	}
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		*concat = src[i];
+		concat++;
+	}
+	*concat = '\0';
+	return (dest);
+}
