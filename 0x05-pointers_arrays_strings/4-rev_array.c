b6c11a4ff9040adbcc2fbfff12d27dda2bae8530
Ken-Mens <kenneth.mensah@gmail.com>
2019-02-22 02:10:53 +0000

    function that reverses content of an array of integers

--- 0x05-pointers_arrays_strings/4-rev_array.c
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -1,20 +1,22 @@
 #include "holberton.h"
 /**
- * reverse_array - reverse the contents of an array 
+ * reverse_array - reverse the contents of an array
  *@a: array that will be reversed
- *@b: size of elements in said array
+ *@n: size of elements in said array
  * Return: 0 on success.
  */
 void reverse_array(int *a, int n)
 {
 	int i, temp;
-	
+
 	i = temp = 0;
 	n = n - 1;
-	for (; i < n; i++, n--)
+	while (i < n)
 	{
 		temp = a[i];
 		a[i] = a[n];
 		a[n] = temp;
+		i++;
+		n--;
 	}
 }
