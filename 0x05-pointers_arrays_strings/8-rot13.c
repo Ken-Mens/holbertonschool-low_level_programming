78aa113e635c40a51f332405c6096032412bc11b
Ken-Mens <kenneth.mensah@gmail.com>
2019-02-22 04:46:01 +0000

    function that encodes string with rot13

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -10,11 +10,16 @@ char *rot13(char *s)
 
 	while (s[i])
 	{
-		if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
-			s[i] += 13;
-		else
-			while ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
-				s[i] -= 13;
+		while ((s[i] >= 'a' && s[i] <= 'z') ||
+		 (s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			if ((s[i] >= 'N' && s[i] <= 'Z') ||
+		(s[i] >= 'n' && s[i] <= 'z'))
+				s[i] = s[i] - 13;
+			else
+				s[i] = s[i] + 13;
+			i++;
+		}
 	i++;
 	}
 	return (s);
