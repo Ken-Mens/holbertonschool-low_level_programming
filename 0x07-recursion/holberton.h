71834ba90112e45726aded5a4fe49d240f7e87fb
Ken-Mens <kenneth.mensah@gmail.com>
2019-02-28 02:21:03 +0000

    add holberton header file

---
+++ 0x07-recursion/holberton.h
@@ -0,0 +1,29 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+
+void _puts_recursion(char *s);
+
+void _print_rev_recursion(char *s);
+
+int _strlen_recursion(char *s);
+
+int factorial(int n);
+
+int _pow_recursion(int x, int y);
+
+int _sqrt_recursion(int n);
+
+int is_prime_number(int n);
+
+int is_palindrome(char *s);
+
+int _helper(int n, int i);
+
+int _putchar(char c);
+
+int _strlen(char *s, int i);
+
+int _helper_palindrome(char *s,  int len);
+
+#endif 
