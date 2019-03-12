4749b5143e4962633ae76adab5578ad5cc97ba00
Ken-Mens <kenneth.mensah@gmail.com>
2019-03-12 00:50:52 +0000

    update dog header

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 #include <stdlib.h>
+#include <stdio.h>
 /**
 * struct dog - structure made for dog
 * @name: character pointer name of dog.
@@ -20,5 +21,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 
 #endif
