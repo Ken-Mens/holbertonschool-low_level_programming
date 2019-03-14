c94bcc4c5986cf078a0993edf9226af65c0cf990
Ken-Mens <kenneth.mensah@gmail.com>
2019-03-14 20:27:35 +0000

    fixed

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -10,18 +10,13 @@ int main(int argc, char *argv[])
 	int (*ptr)(int, int);
 	int x;
 	int y;
+	int result;
+	char foo;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
-	}
-	x = atoi(argv[1]);
-	y = atoi(argv[3]);
-	if ((*argv[2] == '/' || *argv[2] == '%') && y == '0')
-	{
-		printf("Error\n");
-		exit(100);
 	}
 
 	ptr = get_op_func(argv[2]);
@@ -30,6 +25,15 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	printf("%d\n", ptr(x, y));
+	foo = *argv[2];
+	if ((foo == '/' || foo == '%') && !strcmp(argv[3], "0"))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	x = atoi(argv[1]);
+	y = atoi(argv[3]);
+	result = (ptr(x, y));
+	printf("%d\n", result);
 	return (0);
 }
