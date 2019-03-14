b16da699282576bf75098c7e655d3f50a5bbbdf3
Ken-Mens <kenneth.mensah@gmail.com>
2019-03-14 03:20:28 +0000

    main function that ties together all operator functions

---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+/**
+ * main - main function for operators
+ * @argc: arg count
+ * @argv: arguments
+ * Return: Nothing.
+ */
+int main(int argc, char *argv[])
+{
+	int (*ptr)(int, int);
+	int x;
+	int y;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	x = atoi(argv[1]);
+	y = atoi(argv[3]);
+	if ((*argv[2] == '/' || *argv[2] == '%') && y == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	ptr = get_op_func(argv[2]);
+	if (!ptr)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", ptr(x, y));
+	return (0);
+}
