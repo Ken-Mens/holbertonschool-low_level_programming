8b1f16c9fdcf2c7ec7d9cd4ead0de8a986613f85
Ken-Mens <kenneth.mensah@gmail.com>
2019-03-14 03:21:25 +0000

    operators

---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+* op_add - addition function
+* @a: first number
+* @b: second number
+* Return: sum.
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+* op_sub - subtracting function
+* @a: first number
+* @b: second number
+* Return: difference.
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+* op_mul - multiplication function
+* @a: first number
+* @b: second number
+* Return: result of multiplication.
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+* op_div - division function
+* @a: first number
+* @b: second number
+* Return: division result.
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+* op_mod - remainder function
+* @a: first number
+* @b: second number
+* Return: remainder of the division
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
