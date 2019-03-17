c4663aa4161a77e1267c9a21ba51b108ee32d93b
Ken-Mens <kenneth.mensah@gmail.com>
2019-03-17 02:09:37 +0000

    variadic header file

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,12 +1,21 @@
 #ifndef VARIADIC_H
 #define VARIADIC_H
 #include <stdio.h>
-#include <stdarg.h>
+#include <stdlib.h>
+#include <stdarg.h> 
+typedef struct check
+{	
+	char * chk;
+	void (*f)(va_list valist);
+} check_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
-
+void p_char(va_list valist);
+void p_int(va_list valist);
+void p_float(va_list valist);
+void p_string(va_list valist);
 #endif
