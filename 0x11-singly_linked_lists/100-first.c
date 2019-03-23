9d1cd4f424b2e904a6f49af94c55c069b7fe533b
Ken-Mens <kenneth.mensah@gmail.com>
2019-03-23 02:46:47 +0000

    add first printed before main line

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -1,13 +1,10 @@
 #include "lists.h"
 /**
  * Printline - function that prints a line before main function
- *
  * Return: Void.
  */
-void Printline(void) __attribute__((constructor));
-
 void Printline(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my
-	house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
