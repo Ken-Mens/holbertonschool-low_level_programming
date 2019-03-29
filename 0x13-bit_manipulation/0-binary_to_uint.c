b06684d1268b51894c2bef8a4c414d34614bcd84
Ken-Mens <kenneth.mensah@gmail.com>
2019-03-29 02:41:04 +0000

    function fixed for binary to uint

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -7,14 +7,13 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int hex = 0;
-	unsigned int x = 0;
+	unsigned int hex = 0, x = 0;
 
-	if (b == NULL)
+	if (!b)
 	return (0);
 	while (b[x])
 	{
-		if (b[x] != '1' && b[x] != '0')
+		if (b[x] != '0' && b[x] != '1')
 		{
 			return (0);
 		}
@@ -24,7 +23,7 @@ unsigned int binary_to_uint(const char *b)
 			hex += 1;
 		}
 		else if (b[x] == '0')
-		hex = hex << 1;
+		hex  <<= 1;
 		x++;
 	}
 	return (hex);
