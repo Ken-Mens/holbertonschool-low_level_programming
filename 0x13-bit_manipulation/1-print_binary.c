32c328eaedd1aae5f38c6d5b1fc32665f06e906f
Ken-Mens <kenneth.mensah@gmail.com>
2019-03-29 02:55:59 +0000

    function that prints binary fixed

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -6,12 +6,20 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int temp = n;
+	int i = 0;
+	int bin = sizeof(n) * 8 - 1;
 
-	if (temp > 1)
-		print_binary(temp >> 1);
-	if ((temp & 1) == 1)
-		_putchar('1');
-	else
+	if (n == 0)
 		_putchar('0');
+
+	for (; bin >= 0; bin--)
+	{
+		if (n >> bin & 1)
+		{
+			_putchar('1');
+			i++;
+		}
+		else if (i != '\0')
+			_putchar('0');
+	}
 }
