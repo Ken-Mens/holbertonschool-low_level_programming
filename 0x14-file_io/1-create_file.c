c932a9b094805f8a0569dd0dda036d42b14b0c82
Ken-Mens <kenneth.mensah@gmail.com>
2019-04-03 17:59:59 +0000

    function fixed for creating file

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -14,14 +14,9 @@ int create_file(const char *filename, char *text_content)
 	foo = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (foo == -1)
 		return (-1);
-	if (text_content != NULL)
-	{
-		for (; text_content[leng] != '\0'; leng++)
-		{
-			break;
-		}
-		readve = write(foo, text_content, leng);
-	}
+	if (!text_content)
+		return (1);
+	readve = write(foo, text_content, leng);
 	if (readve == -1)
 		return (-1);
 	close(foo);
