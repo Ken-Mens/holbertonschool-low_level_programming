888820a8fde0dadfaefe4e40f91691675e9b602b
Ken-Mens <kenneth.mensah@gmail.com>
2019-04-03 01:35:55 +0000

    function that creates a file I/O

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * create_file - function that creates a file.
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write.
+ * Return: -1 if filename is NULL or 1 on success.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int foo, leng = 0, readve;
+
+	if (filename == NULL)
+		return (-1);
+	foo = open(filename, O_CREAT | O_TRUNC | O_RDWR | S_IRUSR | S_IWUSR);
+	if (foo == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (; text_content[leng] != '\0'; leng++)
+		{
+			break;
+		}
+		readve = write(foo, text_content, leng);
+	}
+	if (readve == -1)
+		return (-1);
+	close(foo);
+	return (1);
+}
