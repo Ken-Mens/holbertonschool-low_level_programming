ffef9c4c0d57a120e56c5155c0c64b83bc50d727
Ken-Mens <kenneth.mensah@gmail.com>
2019-04-03 01:44:28 +0000

    function that appends to file fixed again

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -7,7 +7,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int foo, leng = 0;
+	int foo, leng = 0, wrote;
 
 	if (filename == NULL)
 		return (-1);
@@ -18,7 +18,9 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (1);
 	for (; text_content[leng] != '\0'; leng++)
 		break;
-	write(foo, text_content, leng);
+	wrote = write(foo, text_content, leng);
+	if (wrote == -1)
+	return (-1);
 	close(foo);
 		return (1);
 }
