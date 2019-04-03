3a2beb2e86e82a0a0b2e64c2f96fdbbba3479956
Ken-Mens <kenneth.mensah@gmail.com>
2019-04-03 17:45:24 +0000

    fixed task1

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	foo = open(filename, O_CREAT | O_TRUNC | O_RDWR | S_IRUSR | S_IWUSR);
+	foo = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (foo == -1)
 		return (-1);
 	if (text_content != NULL)
