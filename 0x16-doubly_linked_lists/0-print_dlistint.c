320b462813cfb4d8aadb87fda969ff9aea137ada
Ken-Mens <kenneth.mensah@gmail.com>
2019-05-05 18:12:20 +0000

    doubly linked list printing elements

--- 0x16-doubly_linked_lists/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -8,14 +8,11 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t counter = 0;
 
-	if (h == NULL)
-		return ('\0');
-
 	while (h)
 	{
 		printf("%d \n", h->n);
-		h = h->next;
 		counter++;
+		h = h->next;
 	}
 	return (counter);
 }
