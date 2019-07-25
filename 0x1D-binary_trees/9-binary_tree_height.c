106927f8954f697df330bd0115535ecb4d908f74
Ken-Mens <kenneth.mensah@gmail.com>
2019-07-25 18:24:53 +0000

    find height of binary tree

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -6,17 +6,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left, right;
+	size_t height_l = NULL;
+	size_t height_r = NULL;
 
-	if (tree == NULL || ((tree->left) == NULL && (tree->right) == NULL))
+	if(tree == NULL)
 		return (0);
-		left = binary_tree_height(tree->left);
-		right = binary_tree_height(tree->right);
 
-	if (right > left)
-	{
-		return (left + 1);
-	}
-	else
-		return (right + 1);
+		height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return (height_l > height_r ? height_l : height_r);
 }
+
